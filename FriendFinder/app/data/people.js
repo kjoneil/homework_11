9df63d8bfc630cfc8ea3cdd0ff8b2aa1d05497f1
kjoneil <kjoneil.rw@gmail.com>
2019-04-09 21:13:22 -0500

    Data is being captured by the survey

---
+++ FriendFinder/app/data/people.js
@@ -0,0 +1,9 @@
+var People = [{
+	"name":"Kyle",
+	  "photo":"https://i.kym-cdn.com/entries/icons/original/000/016/546/hidethepainharold.jpg",
+	   "scores":['3','3','2','2','4','4','4','5','2','4']
+}]
+	
+  
+
+module.exports = People;
